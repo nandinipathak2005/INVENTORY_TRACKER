fa21865e0d2e1a6086c3ef87a077f02245fb3db2
Nandini Manoj Pathak <166193813+nandinipathak2005@users.noreply.github.com>
2024-12-08 12:54:11 +0530

    Added files

---
+++ firebase.js
@@ -0,0 +1,22 @@
+
+import { initializeApp } from "firebase/app";
+
+import {getFirestore}   from "firebase/firestore";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD_siQdcQI-NGD20Z0kUkNhC_-VW4vdDZU",
+  authDomain: "inventory-management-48cfc.firebaseapp.com",
+  projectId: "inventory-management-48cfc",
+  storageBucket: "inventory-management-48cfc.appspot.com",
+  messagingSenderId: "214131096979",
+  appId: "1:214131096979:web:2e905b9a1ad279803fbc66",
+  measurementId: "G-7E4PW0S4WK"
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+const firestore = getFirestore(app);
+
+export {firestore}
